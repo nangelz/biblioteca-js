98793911b0527e4a02c06fe5a7a7e8ea79f7c32e
nangelz <nangeljosue8@gmail.com>
2026-08-01 01:20:09 -0300

    prepaçao  das class e functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+opciones = [
+    "1 - Listar livros",
+    "2 - Buscar livro",
+    "3 - Listar livros disponiveis",
+    "4 - Cadastrar livro",
+    "5- Realizar emprestimo",
+    "6 - Realizar emprestimo",
+    "7 - Exibir estatisticas",
+    "0 - Sair"
+];
+
+console.log(opciones);
+const opcion = prompt('escolha uma opçao:');
+
